9416439041c68b150dd041af0a658965d5251e6c
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2016-05-06 17:01:45 +0200

    Correction des bugs avec le PATH absent

--- src/exec/cmd_path.c
+++ src/exec/cmd_path.c
@@ -5,7 +5,7 @@
 ** Login   <hugo.soszynski@epitech.eu>
 **
 ** Started on  Mon Apr 11 11:06:18 2016 Hugo SOSZYNSKI
-** Last update Tue May  3 15:03:31 2016 Hugo SOSZYNSKI
+** Last update Fri May  6 16:59:58 2016 Hugo SOSZYNSKI
 */
 
 #include	<stddef.h>
@@ -25,28 +25,38 @@ static char	*get_next_path(char *path, int *cpt)
   return (&(path[*cpt]));
 }
 
-static int	check_path(char **cmd, char *path)
+static int	test_tmp_path(char *tmp, char **cmd, int cmd_len)
+{
+  if ((tmp = my_path_dup(tmp, *cmd, cmd_len)) == NULL)
+    return (error_msg("Error: check_path: Can't duplicate the path"));
+  if (access(tmp, X_OK) == 0)
+    {
+      free(*cmd);
+      *cmd = tmp;
+      return (SUCCESS);
+    }
+  return (ERROR);
+}
+
+static int	check_path(char **cmd, t_data *exec)
 {
   int		cpt;
   int		cmd_len;
   char		*tmp;
+  char		*path;
 
   cpt = 0;
   if ((cmd_len = my_strlen(*cmd)) == -1)
     return (ERROR);
   if (access(cmd[0], X_OK) == 0)
     return (SUCCESS);
+  if ((path = my_getenv(exec->env, "PATH")) == NULL)
+    return (error_msg("Error: test_path: Can't access PATH env variable"));
   tmp = path;
   while (tmp != NULL)
     {
-      if ((tmp = my_path_dup(tmp, *cmd, cmd_len)) == NULL)
-	return (error_msg("Error: check_path: Can't duplicate the path"));
-      if (access(tmp, X_OK) == 0)
-	{
-	  free(*cmd);
-	  *cmd = tmp;
-	  return (SUCCESS);
-	}
+      if (test_tmp_path(tmp, cmd, cmd_len) == SUCCESS)
+	return (SUCCESS);
       free(tmp);
       tmp = get_next_path(path, &cpt);
     }
@@ -57,19 +67,16 @@ int		test_path(t_data *exec)
 {
   t_list	*current;
   t_list	*pipe;
-  char		*path;
 
-  if ((path = my_getenv(exec->env, "PATH")) == NULL)
-    return (error_msg("Error: test_path: Can't access PATH env variable"));
   current = exec->list;
   while (current != NULL)
     {
-      if (is_built_in(current) && check_path(&(current->cmd[0]), path))
+      if (is_built_in(current) && check_path(&(current->cmd[0]), exec))
 	current->correct = false;
       pipe = current->pipe;
       while (pipe != NULL)
 	{
-	  if (is_built_in(current) && check_path(&(pipe->cmd[0]), path))
+	  if (is_built_in(current) && check_path(&(pipe->cmd[0]), exec))
 	    pipe->correct = false;
 	  pipe = pipe->pipe;
 	}
