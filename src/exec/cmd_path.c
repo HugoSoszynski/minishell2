36390758b9249cbc518175904be9238357e6ffd7
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2016-05-06 17:43:51 +0200

    Correction exec de chemins relatifs faux

--- src/exec/cmd_path.c
+++ src/exec/cmd_path.c
@@ -5,7 +5,7 @@
 ** Login   <hugo.soszynski@epitech.eu>
 **
 ** Started on  Mon Apr 11 11:06:18 2016 Hugo SOSZYNSKI
-** Last update Fri May  6 16:59:58 2016 Hugo SOSZYNSKI
+** Last update Fri May  6 17:43:10 2016 Hugo SOSZYNSKI
 */
 
 #include	<stddef.h>
@@ -25,17 +25,32 @@ static char	*get_next_path(char *path, int *cpt)
   return (&(path[*cpt]));
 }
 
-static int	test_tmp_path(char *tmp, char **cmd, int cmd_len)
+static int	test_tmp_path(char **tmp, char **cmd, int cmd_len)
 {
-  if ((tmp = my_path_dup(tmp, *cmd, cmd_len)) == NULL)
+  if ((*tmp = my_path_dup(*tmp, *cmd, cmd_len)) == NULL)
     return (error_msg("Error: check_path: Can't duplicate the path"));
-  if (access(tmp, X_OK) == 0)
+  if (access(*tmp, X_OK) == 0)
     {
       free(*cmd);
-      *cmd = tmp;
+      *cmd = *tmp;
       return (SUCCESS);
     }
+  free(*tmp);
   return (ERROR);
+}
+
+static int	check_slash(char *cmd)
+{
+  int		cpt;
+
+  cpt = 0;
+  while (cmd[cpt] != '\0')
+    {
+      if (cmd[cpt] == '/')
+	return (ERROR);
+      cpt++;
+    }
+  return (SUCCESS);
 }
 
 static int	check_path(char **cmd, t_data *exec)
@@ -45,42 +60,46 @@ static int	check_path(char **cmd, t_data *exec)
   char		*tmp;
   char		*path;
 
-  cpt = 0;
-  if ((cmd_len = my_strlen(*cmd)) == -1)
+  if ((cpt = 0) == 0 && (cmd_len = my_strlen(*cmd)) == -1)
     return (ERROR);
-  if (access(cmd[0], X_OK) == 0)
-    return (SUCCESS);
-  if ((path = my_getenv(exec->env, "PATH")) == NULL)
-    return (error_msg("Error: test_path: Can't access PATH env variable"));
-  tmp = path;
-  while (tmp != NULL)
+  if (check_slash(*cmd) == ERROR)
     {
-      if (test_tmp_path(tmp, cmd, cmd_len) == SUCCESS)
+      if (access(cmd[0], X_OK) == 0)
 	return (SUCCESS);
-      free(tmp);
-      tmp = get_next_path(path, &cpt);
+    }
+  else
+    {
+      if ((path = my_getenv(exec->env, "PATH")) == NULL)
+	return (error_msg("Error: test_path: Can't access PATH env variable"));
+      tmp = path;
+      while (tmp != NULL)
+	{
+	  if (test_tmp_path(&tmp, cmd, cmd_len) == SUCCESS)
+	    return (SUCCESS);
+	  tmp = get_next_path(path, &cpt);
+	}
     }
   return (ERROR);
 }
 
-int		test_path(t_data *exec)
-{
-  t_list	*current;
-  t_list	*pipe;
+  int		test_path(t_data *exec)
+  {
+    t_list	*current;
+    t_list	*pipe;
 
-  current = exec->list;
-  while (current != NULL)
-    {
-      if (is_built_in(current) && check_path(&(current->cmd[0]), exec))
-	current->correct = false;
-      pipe = current->pipe;
-      while (pipe != NULL)
-	{
-	  if (is_built_in(current) && check_path(&(pipe->cmd[0]), exec))
-	    pipe->correct = false;
-	  pipe = pipe->pipe;
-	}
-      current = current->next;
-    }
-  return (SUCCESS);
-}
+    current = exec->list;
+    while (current != NULL)
+      {
+	if (is_built_in(current) && check_path(&(current->cmd[0]), exec))
+	  current->correct = false;
+	pipe = current->pipe;
+	while (pipe != NULL)
+	  {
+	    if (is_built_in(current) && check_path(&(pipe->cmd[0]), exec))
+	      pipe->correct = false;
+	    pipe = pipe->pipe;
+	  }
+	current = current->next;
+      }
+    return (SUCCESS);
+  }
